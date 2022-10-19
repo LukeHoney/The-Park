59066fb63da052f4cf95ec69d4917ad110e0d07d
LukeHoney <2248102@swansea.ac.uk>
2022-10-19 11:24:05 +0100

    Add HelloScript.cs <Luke Honey>

---
+++ Assets/Scripts/HelloConsole.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloConsole : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("HelloConsole.cs");
+        Debug.Log("Luke Honey");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
